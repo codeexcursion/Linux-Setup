1cae1b4939ed01eccbac84798c6445afd3b373a9
Chris Lynch <chrislynch42@yahoo.com>
2015-06-09 17:05:10 -0400

    Needed to specify the home directory when creating the linked folders

--- make_vim_links.sh
+++ make_vim_links.sh
@@ -1,3 +1,3 @@
-ln -s ~/source/vim/configuration/.vimrc  .vimrc
+ln -s ~/source/vim/configuration/.vimrc  ~/.vimrc
 
-ln -s ~/source/vim/configuration/.vim  .vim
+ln -s ~/source/vim/configuration/.vim  ~/.vim
